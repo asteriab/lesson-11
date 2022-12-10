d7bcbcc878849b7039be7172782bd3b9750a4eb7
Mariana <mariana.balanchuk@gmail.com>
2022-12-10 09:22:35 +0100

    added task 4

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-// Task1
+// // Task1
 
 let number = prompt("Enter your number:");
 number *= number;
 alert(`The square number is ${number}`);
 
-// Task2
+// // Task2
 
-let username = prompt("Введіть ім'я:");
-let sum = prompt("Введіть суму депозиту:");
-let term = prompt("Введіть термін депозиту (місяців):");
-let percent = 0.2;
-let profit = (percent / 12) * sum * term;
-alert(
-  `Шановний(а) ${username}, Ви внесли ${sum} грн, під 20% річних, на термін ${term} місяців. За цей період часу Ви заробите ${profit} грн.`
-);
+// let username = prompt("Введіть ім'я:");
+// let sum = prompt("Введіть суму депозиту:");
+// let term = prompt("Введіть термін депозиту (місяців):");
+// let percent = 0.2;
+// let profit = (percent / 12) * sum * term;
+// alert(
+//   `Шановний(а) ${username}, Ви внесли ${sum} грн, під 20% річних, на термін ${term} місяців. За цей період часу Ви заробите ${profit} грн.`
+// );
 
-// Task3
+// // Task3
 
-let number1 = Number(prompt("Enter the first number:"));
-let number2 = Number(prompt("Enter the second number:"));
-let suma = number1 + number2;
-let difference = number1 - number2;
-let product = number1 * number2;
-let quotient = number1 / number2;
+// let number1 = Number(prompt("Enter the first number:"));
+// let number2 = Number(prompt("Enter the second number:"));
+// let suma = number1 + number2;
+// let difference = number1 - number2;
+// let product = number1 * number2;
+// let quotient = number1 / number2;
 
-alert(`The sum is ${suma}.
-The difference is ${difference}
-The product is ${product}
-The quotient is ${quotient}`);
+// alert(`The sum is ${suma}.
+// The difference is ${difference}
+// The product is ${product}
+// The quotient is ${quotient}`);
+
+// Task4
+
+// let age = Number(prompt("Enter your age:"));
+// if (age >= 18) {
+//   alert("You can join!");
+// } else {
+//   alert("You cannot join :(");
+// }
